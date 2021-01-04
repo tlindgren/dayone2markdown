31063df60e80a11adab8dd8272e87fbccfdecf92
Tim Lindgren <timothy.lindgren@bc.edu>
2021-01-03 22:03:06 -0600

    rewrite with YAML and prep for filename-creation date update

--- do2md.py
+++ do2md.py
@@ -2,6 +2,7 @@
 import json
 import codecs
 import sys
+import pytz
 from datetime import datetime
 
 tag_symbol = "#"
@@ -29,25 +30,74 @@ all = json.load(fp)
 #entry2md is a function that will convert one single DayOne journal entry as
 #a markdown file.
 def entry2md(entry):
-        date = datetime.strptime(entry['creationDate'],'%Y-%m-%dT%H:%M:%SZ')
+
+        #Reference: working with dates in Python: https://techblog.thescore.com/2015/11/03/timezones-in-python/
+
+        #get the original timezone
+        timezone = entry['timeZone']
+        #set the timezone to UTC
+        original_date = datetime.strptime(entry['creationDate'],'%Y-%m-%dT%H:%M:%SZ')
+        utc = pytz.timezone('UTC')
+        aware_date = utc.localize(original_date)
+        aware_date.tzinfo # <UTC>
+        
+        #set the timezone to the original
+        date = pytz.timezone(timezone)
+        date = aware_date.astimezone(date)
+        date.tzinfo
+
 	#date as the filename
         #it is assumed that there cannot be two entries in the same second.
         #If this is nevertheless the case, one of the two will be lost
-        filename = date.strftime("%Y-%b-%d_%H-%m-%S")+".md"
+        
+        #filename = date.strftime("%Y-%m-%dT%H-%M-%S")+".md"
+        # V2 - for use with script that changes creation date to filename
+        filename = date.strftime("%Y-%m-%d %H.%M")+".md"
+
         print filename
+
+        #Add Yaml frontmatter
+        text = "---\n"
+        
+        #title
+        title = date.strftime("%A, %B %d, %Y %I:%M%p")
+        text += "Title: "+title+"\n"
+
+        #date
+        yaml_date = date.strftime("%Y-%m-%d")
+        text += "Date: "+str(yaml_date)+"\n"
+
+        #time
+        yaml_time = date.strftime("%H:%M:%S")
+        text += "Time: "+str(yaml_time)+"\n"
+
+        #location
+        place = ""
+        if 'location' in entry.keys():
+            location = entry['location']
+            #print(location)
+            place = ""
+            # removed address = 'placeName',
+            for t in ['localityName','administrativeArea','country']:
+                if t in location.keys():
+                    place += location[t]+", "
+            #if 'longitude' in location.keys() and 'latitude' in location.keys():
+                #place += ""
+                #place += "long: "+str(location['longitude'])
+                #place += ", lat: "+str(location['latitude'])
+  
+        text += "Location: "+place+"\n"
+
+        text += "---\n\n"
+
+        #Add body of entry
+        
         #Add date as the title
-        text = "# "+str(date)+"\n\n"+entry['text']
+        # text += "# "+str(date)+"\n\n"+entry['text']
+
 	#for some reason, "." and () are escaped
 	text = text.replace("\.",".").replace("\(","(")\
                    .replace("\)",")").replace("\-","-")
-	tags = ""
-	#we add tags at the ends of each entry with the tag symbol
-        #but only if the tag is not already in the text
-	if 'tags' in entry.keys():
-		for t in entry['tags']:
-		    tag = " %s%s" %(tag_symbol,t)
-                    if tag not in text:
-                        tags += tag
 
 	#we convert the dayone-moment photo link to local markdown link
         photos = dict()
@@ -61,21 +111,18 @@ def entry2md(entry):
 			original = "![](dayone-moment://%s)" %ph
 			new = "![](photos/%s)" %photos[ph]
 			text = text.replace(original,new)
-        #we add tags at the end of the text
+        
+        #add Tags
+        tags = ""
+	#we add tags at the ends of each entry with the tag symbol
+        #but only if the tag is not already in the text
+	if 'tags' in entry.keys():
+		for t in entry['tags']:
+		    tag = " %s%s" %(tag_symbol,t)
+                    if tag not in text:
+                        tags += tag
 	text += "\n\n%s" %tags
-        #we add location
-        if 'location' in entry.keys():
-            location = entry['location']
-            #print(location)
-            place = "\n\n"
-            for t in ['placeName','localityName','administrativeArea','country']:
-                if t in location.keys():
-                    place += location[t]+"\n"
-            if 'longitude' in location.keys() and 'latitude' in location.keys():
-                place += ""
-                place += "long: "+str(location['longitude'])
-                place += ", lat: "+str(location['latitude'])
-            text += place
+        
 	fp = codecs.open(filename,'w','utf-8')
         fp.write(text)
         fp.close()
